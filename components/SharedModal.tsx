4f48774cb82ba80469820285c03fbf608b970fc0
周颂杰 <mac@MacdeMacBook-Pro.local>
2023-11-21 00:39:01 +0800

    modified:   components/SharedModal.tsx

--- components/SharedModal.tsx
+++ components/SharedModal.tsx
@@ -14,7 +14,6 @@ import { variants } from '../utils/animationVariants'
 import downloadPhoto from '../utils/downloadPhoto'
 import { range } from '../utils/range'
 import type { ImageProps, SharedModalProps } from '../utils/types'
-import Twitter from './Icons/Twitter'
 
 export default function SharedModal({
   index,
