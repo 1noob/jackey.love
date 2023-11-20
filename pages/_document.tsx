cd9aef38fa1000dbc6d4cbb29f34902ef9455f33
周颂杰 <mac@MacdeMacBook-Pro.local>
2023-11-21 00:49:44 +0800

    modified:   pages/_document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+            <title>Images</title>
           <link rel="icon" href="FinderIcon.png" />
         </Head>
         <body className="bg-black antialiased">
