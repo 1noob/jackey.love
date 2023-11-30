693e2bb00e8a473ea7ac003b0513d5d8f1073d6f
周颂杰 <mac@MacdeMacBook-Pro.local>
2023-12-01 00:41:05 +0800

    modified:   pages/_document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-            <link
-                rel="apple-touch-icon"
-                sizes="180x180"
-                href="/apple-touch-icon.jpg"
-            />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.jpg"/>
             <link rel="icon" href="/FinderIcon.png" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
             <title>Images</title>
         </Head>
         <body className="bg-black antialiased">
