0109b85975fe412bff05f3f41711c34f1d19b03e
周颂杰 <31345708+1noob@users.noreply.github.com>
2023-12-29 03:18:14 +0800

    modified:   pages/_app.tsx 	new file:   public/opengraph-image.jpg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <>
           <NextUIProvider>
               <Head>
+                  <meta property="og:title" content="JackeyLove"/>
+                  <meta property="og:site_name" content="JackeyLove"/>
+                  <meta property="og:description" content="AD Carry"/>
+                  <meta property="og:url" content="https://jackey.love/"/>
+                  <meta property="og:image" content="https://jackey.love/opengraph-image.jpg"/>
                   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.jpg"/>
                   <link rel="icon" type="image/x-icon" href="favicon.ico" />
                   <meta name="apple-mobile-web-app-capable" content="yes" />
