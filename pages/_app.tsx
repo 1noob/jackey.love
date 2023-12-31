6bd396f3a28ff95c266f3537fd634e0446ecc620
周颂杰 <31345708+1noob@users.noreply.github.com>
2023-12-31 07:51:02 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,24 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <>
           <NextUIProvider>
               <Head>
+
+                  <title>JackeyLove</title>
+                  <meta name="description" content="League of Legends esports player"/>
+
+                  <meta property="og:url" content="https://www.jackey.love"/>
+                  <meta property="og:type" content="website"/>
                   <meta property="og:title" content="JackeyLove"/>
-                  <meta property="og:site_name" content="JackeyLove"/>
-                  <meta property="og:description" content="AD Carry"/>
-                  <meta property="og:url" content="https://jackey.love/"/>
+                  <meta property="og:description" content="League of Legends esports player"/>
                   <meta property="og:image" content="https://jackey.love/opengraph-image.png"/>
+
+                  <meta name="twitter:card" content="summary_large_image"/>
+                  <meta property="twitter:domain" content="jackey.love"/>
+                  <meta property="twitter:url" content="https://www.jackey.love"/>
+                  <meta name="twitter:title" content="JackeyLove"/>
+                  <meta name="twitter:description" content="League of Legends esports player"/>
+                  <meta name="twitter:image" content="https://jackey.love/opengraph-image.png"/>
+
+
                   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
                   <link rel="icon" type="image/x-icon" href="/favicon.ico" />
                   <meta name="apple-mobile-web-app-capable" content="yes" />
