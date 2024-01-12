2251709f129e1be9de93f106e2dce5e871de09d2
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-01-12 16:39:29 +0800

    modified:   components/Box.tsx 	modified:   styles/index.css

--- components/Box.tsx
+++ components/Box.tsx
@@ -8,7 +8,7 @@ interface BoxProps {
 
 const Box: React.FC<BoxProps> = ({ children, className }) => {
   return (
-    <Card isBlurred shadow="none" radius="sm" className={"bg-card"}
+    <Card isBlurred shadow="sm" radius="sm" className={"bg-card"}
         classNames={{
             body: "overflow-hidden"
         }}
