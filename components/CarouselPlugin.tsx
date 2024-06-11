a90ba19cb71dd20c783926f85440c37379dbbb8b
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-06-11 14:07:56 +0800

    Update CarouselPlugin.tsx

--- components/CarouselPlugin.tsx
+++ components/CarouselPlugin.tsx
@@ -26,7 +26,7 @@ const CarouselPlugin = () => {
       onMouseLeave={plugin.current.reset}
     >
       <CarouselContent>
-        <CarouselItem className="pb-4">
+        <CarouselItem key={1} className="pb-4">
           <Image
             rel="preload"
             classNames={{
@@ -40,20 +40,20 @@ const CarouselPlugin = () => {
             alt={"JackeyLove, 喻文波, Yu-WenBo, 阿水, 水子哥, 哥哥"}
           />
         </CarouselItem>
-        <CarouselItem className="pb-4">
-          <iframe
-            className={"shadow-lg w-full rounded-2xl h-full"}
-            allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
-            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-            src={
-              `https://embed.music.apple.com/cn/playlist/jackeylove-live/pl.u-gxbll0JC5vEGkPj?theme=` +
-              systemTheme
-            }
-          />
+        <CarouselItem key={2} className="pb-4">
+            <iframe
+              className={"shadow-lg w-full rounded-2xl h-full"}
+              allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src={
+                `https://embed.music.apple.com/cn/playlist/jackeylove-live/pl.u-gxbll0JC5vEGkPj?theme=` +
+                systemTheme
+              }
+            />
         </CarouselItem>
       </CarouselContent>
-      <CarouselPrevious className="left-0"/>
-      <CarouselNext className="right-0"/>
+      <CarouselPrevious className="left-0" />
+      <CarouselNext className="right-0" />
     </Carousel>
   );
 };
