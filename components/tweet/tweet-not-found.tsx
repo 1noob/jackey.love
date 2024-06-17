f4f65ccd960e4bed246cf7ec9bf7f32740ac5fa1
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-06-17 16:11:25 +0800

    Update tweet-not-found.tsx

--- components/tweet/tweet-not-found.tsx
+++ components/tweet/tweet-not-found.tsx
@@ -9,6 +9,7 @@ export const TweetNotFound = (_props: Props) => (
     <div className={"notfound-root"}>
       <h3>Tweet not found</h3>
       <p>The embedded tweet could not be found…</p>
+      <p>Maybe you need a VPN...</p>
     </div>
   </TweetContainer>
 );
