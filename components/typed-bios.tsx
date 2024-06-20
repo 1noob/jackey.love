8e8f03697f96404f1ae22fee31742f6127f2bdcb
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-06-20 12:54:39 +0800

    Update typed-bios.tsx

--- components/typed-bios.tsx
+++ components/typed-bios.tsx
@@ -39,7 +39,7 @@ const TypedBios: React.FC<Props> = ({ className }) => {
   return (
     <div
       className={cn(
-        "z-[999] grid grid-cols-6 px-3 py-2 mx-1 border border-dashed border-gray-300 dark:border-0 md:border-0 rounded-xl bg-card md:bg-blur md:backdrop-blur-2xl bottom-1 mobile:absolute",
+        "z-[999] grid grid-cols-6 px-3 py-2 mx-1 border border-dashed border-gray-300 dark:border-0 md:border-0 rounded-b-xl md:rounded-xl bg-card md:bg-blur md:backdrop-blur-2xl bottom-1 mobile:absolute",
         className
       )}
     >
