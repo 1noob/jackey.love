3aa8edfd38896c9d4f247f4d24dd379a81c52ed1
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-06-27 15:59:51 +0800

    border-gray-400

--- components/tweet/tweet-container.tsx
+++ components/tweet/tweet-container.tsx
@@ -6,7 +6,7 @@ type Props = { className?: string; children: ReactNode };
 export const TweetContainer = ({ className, children }: Props) => (
   <div
     className={clsx(
-      "react-tweet-theme container-root h-[450px] border-dashed border-gray-300 border dark:border-0",
+      "react-tweet-theme container-root h-[450px] border-dashed border-gray-400 border dark:border-0",
       className
     )}
   >
