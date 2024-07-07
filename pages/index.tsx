9f7203666f0fe0a5abcf6122baa1a2f3bc1b46a1
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-07-07 23:53:59 +0800

    bug fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -152,10 +152,12 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         nodeRef={nodeRef}
       >
         <div className="loading" ref={nodeRef}>
-          <JackeyLoveIcon
-            size={300}
-            className="dark:brightness-150 w-[50%] md:w-[35%] lg:w-[20%] m-auto"
-          />
+          <div className="w-full h-svh place-content-center">
+            <JackeyLoveIcon
+              size={300}
+              className="dark:brightness-150 w-[50%] md:w-[35%] lg:w-[20%] m-auto"
+            />
+          </div>
         </div>
       </CSSTransition>
     </>
