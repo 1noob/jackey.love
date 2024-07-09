25425307e1177509a5956622cbd7f6810bfdd7c3
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-07-09 23:18:49 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>JackeyLove</title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover"
         />
       </Head>
       <NextUIProvider>
