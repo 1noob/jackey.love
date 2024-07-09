cf91e3881d4db1a52896a2f6171ed856a3da0a46
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-07-09 23:00:16 +0800

    bug fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -151,7 +151,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         unmountOnExit
         nodeRef={nodeRef}
       >
-        <div className="bg-blur backdrop-blur-xl absolute w-full h-lvh z-[999] place-content-center" ref={nodeRef}>
+        <div className="bg-blur backdrop-blur-xl absolute top-0 w-full h-lvh z-[999] place-content-center" ref={nodeRef}>
           <JackeyLoveIcon
             size={300}
             className="dark:brightness-150 w-[50%] md:w-[35%] lg:w-[20%] mx-auto"
