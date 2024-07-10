ad2e387b685a11045c22896bac13a5f57a09b537
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-07-10 09:26:48 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -151,16 +151,19 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         unmountOnExit
         nodeRef={nodeRef}
       > */}
-      {/* 使用 safe-area 进行定位会导致抖动 */}
-        <div
-          className="bg-blur backdrop-blur-xl fixed top-0 w-full h-lvh z-[999] border border-red-500 "
-          ref={nodeRef}
-        >
-          <JackeyLoveLogo
-            size={300}
-            className="h-lvh dark:brightness-150 w-[40%] md:w-[30%] lg:w-[20%] m-auto"
-          />
-        </div>
+      {/* 
+        使用 safe-area 进行定位会导致抖动
+        使用 lvh 会先出现 svh 再延伸为 lvh
+       */}
+      <div
+        className="bg-blur backdrop-blur-xl fixed top-0 w-full h-lvh z-[999] border border-red-500 "
+        ref={nodeRef}
+      >
+        <JackeyLoveLogo
+          size={300}
+          className="h-svh absolute top-0 left-[30%] md:left-[35%] lg:left-[40%] ldark:brightness-150 w-[40%] md:w-[30%] lg:w-[20%] m-auto"
+        />
+      </div>
       {/* </CSSTransition> */}
     </>
   );
