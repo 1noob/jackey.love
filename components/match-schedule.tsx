c76dee5b209b2a67d45ccee144826a01965f78b9
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-08-08 01:41:52 +0800

    update

--- components/match-schedule.tsx
+++ components/match-schedule.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Divider, Image } from "@nextui-org/react";
 import useSWR from "swr";
 import Box from "./Box";
+import { EDG, FPX, JDG, LGD, LNG, LPL, NIP, OMG, RA, RNG, TES, TT, WBG, WE, UP } from "@/public/img/team/team-icon";
 
 interface Props {
   url: string;
@@ -10,6 +11,24 @@ interface Props {
 const MatchSchedule: React.FC<Props> = ({ url }) => {
   const fetcher = (arg: string) => fetch(arg).then((res) => res.json());
   const { data } = useSWR(url, fetcher);
+
+  const Icon = {
+    "Royal Never Give Up" : <RNG size={35} className="w-full"/>,
+    "FunPlus Phoenix" : <FPX size={35} className="w-full"/>,
+    "Top Esports" : <TES size={35} className="w-full"/>,
+    "JD Gaming": <JDG size={35} className="w-full"/>,
+    "TBD": <LPL size={35} className="w-full"/>,
+    "EDward Gaming": <EDG size={35} className="w-full"/>,
+    "LNG Gaming": <LNG size={35} className="w-full"/>,
+    "Weibo Gaming": <WBG size={35} className="w-full"/>,
+    "Team WE": <WE size={35} className="w-full"/>,
+    "Ninjas in Pyjamas.CN": <NIP size={35} className="w-full"/>,
+    "Rare Atom": <RA size={35} className="w-full"/>,
+    "ThunderTalk Gaming": <TT size={35} className="w-full"/>,
+    "LGD Gaming": <LGD size={35} className="w-full"/>,
+    "Oh My God": <OMG size={35} className="w-full"/>,
+    "Ultra Prime": <UP size={35} className="w-full"/>,
+  }
 
   if (data != undefined) {
     return (
@@ -25,16 +44,8 @@ const MatchSchedule: React.FC<Props> = ({ url }) => {
               >
                 <span>{item["StandardName"]}</span>
                 <div className="grid grid-cols-3">
-                  <div className="grid grid-cols-1 justify-center w-full">
-                    <Image
-                      src={`/img/team/${item["Team1"]}.png`}
-                      classNames={{
-                        wrapper: "h-[40px] min-w-full place-content-center dark:bg-gray-200 rounded-full",
-                      }}
-                      className="m-auto"
-                      radius="none"
-                      shadow="none"
-                    />
+                  <div className="grid grid-cols-1 justify-center w-full gap-1">
+                    {Icon[item["Team1"]]}
                     <span className="col-span-2">{item["Team1"]}</span>
                   </div>
                   <div className="place-content-center grid grid-cols-1">
@@ -49,16 +60,8 @@ const MatchSchedule: React.FC<Props> = ({ url }) => {
                     </div>
                     <span>BO{item["BestOf"]}</span>
                   </div>
-                  <div className="grid grid-cols-1 justify-center w-full">
-                    <Image
-                      src={`/img/team/${item["Team2"]}.png`}
-                      classNames={{
-                        wrapper: "h-[40px] min-w-full place-content-center dark:bg-gray-200 rounded-full",
-                      }}
-                      radius="none"
-                      shadow="none"
-                      className="m-auto"
-                    />
+                  <div className="grid grid-cols-1 justify-center w-full gap-1">
+                    {Icon[item["Team2"]]}
                     <span>{item["Team2"]}</span>
                   </div>
                 </div>
