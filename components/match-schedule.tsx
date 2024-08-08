28ffb2e3aaed12f752f6d304eefa64c8ca8a92d3
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-08-08 14:42:27 +0800

    png to svg

--- components/match-schedule.tsx
+++ components/match-schedule.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Divider, Image } from "@nextui-org/react";
 import useSWR from "swr";
 import Box from "./Box";
-import { EDG, FPX, JDG, LGD, LNG, LPL, NIP, OMG, RA, RNG, TES, TT, WBG, WE, UP } from "@/public/img/team/team-icon";
+import { EDG, FPX, JDG, LGD, LNG, LPL, NIP, OMG, RA, RNG, TES, TT, WBG, WE, UP, BLG, IG, AL } from "@/public/img/team/team-icon";
 
 interface Props {
   url: string;
@@ -13,21 +13,24 @@ const MatchSchedule: React.FC<Props> = ({ url }) => {
   const { data } = useSWR(url, fetcher);
 
   const Icon = {
-    "Royal Never Give Up" : <RNG size={35} className="w-full"/>,
-    "FunPlus Phoenix" : <FPX size={35} className="w-full"/>,
-    "Top Esports" : <TES size={35} className="w-full"/>,
-    "JD Gaming": <JDG size={35} className="w-full"/>,
-    "TBD": <LPL size={35} className="w-full"/>,
-    "EDward Gaming": <EDG size={35} className="w-full"/>,
-    "LNG Gaming": <LNG size={35} className="w-full"/>,
-    "Weibo Gaming": <WBG size={35} className="w-full"/>,
-    "Team WE": <WE size={35} className="w-full"/>,
-    "Ninjas in Pyjamas.CN": <NIP size={35} className="w-full"/>,
-    "Rare Atom": <RA size={35} className="w-full"/>,
-    "ThunderTalk Gaming": <TT size={35} className="w-full"/>,
-    "LGD Gaming": <LGD size={35} className="w-full"/>,
-    "Oh My God": <OMG size={35} className="w-full"/>,
-    "Ultra Prime": <UP size={35} className="w-full"/>,
+    "Royal Never Give Up" : <RNG size={40} className="w-full py-1.5"/>,
+    "FunPlus Phoenix" : <FPX size={40} className="w-full py-2"/>,
+    "Top Esports" : <TES size={40} className="w-full py-0.5"/>,
+    "JD Gaming": <JDG size={40} className="w-full py-1"/>,
+    "TBD": <LPL size={40} className="w-full py-1"/>,
+    "EDward Gaming": <EDG size={40} className="w-full"/>,
+    "LNG Gaming": <LNG size={40} className="w-full"/>,
+    "Weibo Gaming": <WBG size={40} className="w-full"/>,
+    "Team WE": <WE size={40} className="w-full"/>,
+    "Ninjas in Pyjamas.CN": <NIP size={40} className="w-full py-0.5"/>,
+    "Rare Atom": <RA size={40} className="w-full"/>,
+    "ThunderTalk Gaming": <TT size={40} className="w-full"/>,
+    "LGD Gaming": <LGD size={40} className="w-full"/>,
+    "Oh My God": <OMG size={40} className="w-full"/>,
+    "Ultra Prime": <UP size={40} className="w-full"/>,
+    "Bilibili Gaming": <BLG size={40} className="w-full py-2"/>,
+    "Invictus Gaming": <IG size={40} className="w-full"/>,
+    "Anyone's Legend": <AL size={40} className="w-full py-1"/>,
   }
 
   if (data != undefined) {
@@ -40,13 +43,13 @@ const MatchSchedule: React.FC<Props> = ({ url }) => {
             return (
               <div
                 key={index}
-                className="text-center grid gap-2 bg-gray-100 dark:bg-gray-900 rounded-[12px] p-3"
+                className="text-center grid gap-2 bg-blue-50 dark:bg-gray-900 rounded-[12px] p-3"
               >
                 <span>{item["StandardName"]}</span>
                 <div className="grid grid-cols-3">
-                  <div className="grid grid-cols-1 justify-center w-full gap-1">
+                  <div className="grid grid-cols-1 justify-center w-full">
                     {Icon[item["Team1"]]}
-                    <span className="col-span-2">{item["Team1"]}</span>
+                    <span className="uppercase">{item["Team1"]}</span>
                   </div>
                   <div className="place-content-center grid grid-cols-1">
                     <div className="text-4xl">
@@ -60,9 +63,9 @@ const MatchSchedule: React.FC<Props> = ({ url }) => {
                     </div>
                     <span>BO{item["BestOf"]}</span>
                   </div>
-                  <div className="grid grid-cols-1 justify-center w-full gap-1">
+                  <div className="grid grid-cols-1 justify-center w-full">
                     {Icon[item["Team2"]]}
-                    <span>{item["Team2"]}</span>
+                    <span className="uppercase">{item["Team2"]}</span>
                   </div>
                 </div>
                 <span>{item["DateTime CST"]}</span>
--- components/statistics.tsx
+++ components/statistics.tsx
@@ -20,13 +20,13 @@ const Stat: React.FC<Props> = ({ title, url }) => {
         <Divider className={"my-4 md:h-0.5"} />
         <div className="grid gap-4">
           <div className="flex justify-between gap-2">
-            <div className="bg-blue-500 dark:bg-blue-950 place-content-center px-2 rounded-md grow text-center">
+            <div className="bg-blue-50 dark:bg-blue-950 place-content-center p-2 rounded-md grow text-center">
               KDA:{data[0]["kills"]}/{data[0]["deaths"]}/{data[0]["assists"]}
             </div>
-            <div className="bg-blue-500 dark:bg-blue-950 place-content-center px-2 rounded-md grow text-center">
+            <div className="bg-blue-50 dark:bg-blue-950 place-content-center p-2 rounded-md grow text-center">
               Wins:{data[0]["wins"]}
             </div>
-            <div className="bg-blue-500 dark:bg-blue-950 place-content-center px-2 rounded-md grow text-center">
+            <div className="bg-blue-50 dark:bg-blue-950 place-content-center p-2 rounded-md grow text-center">
               Games:{data[0]["total"]}
             </div>
           </div>
@@ -54,7 +54,7 @@ const Stat: React.FC<Props> = ({ title, url }) => {
             })}
           </div>
         </div>
-        <span className="pt-7 text-[10px] md:font-light text-gray-400 md:text-gray-600 md:dark:text-gray-400 text-right">
+        <span className="pt-3 text-[10px] md:font-light text-gray-400 md:text-gray-600 md:dark:text-gray-400 text-right">
           {title === "LPL"
             ? 'Exclude "Regional Finals" and "Demacia Cup"'
             : 'Include "World Finals", "MSI", "RR" and "EWC"'}
