e3379f37dfc411f8c072134b4925897f7238cb94
周颂杰 <31345708+1noob@users.noreply.github.com>
2025-02-06 16:08:09 +0800

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -59,7 +59,7 @@ const data = {
     },
     {
       one: "Country",
-      two: <span className="flex gap-1"><CNIcon />China</span>,
+      two: <span className="flex gap-2"><CNIcon className="pb-0.5"/>China</span>,
     },
     {
       one: "Birthday",
