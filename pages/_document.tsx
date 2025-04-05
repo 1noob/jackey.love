c1fbc1176527437b9550149205182d2fc7394bab
song <31345708+1noob@users.noreply.github.com>
2025-04-05 08:48:00 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,6 +38,7 @@ class MyDocument extends Document {
           />
           <link rel="icon" type="image/x-icon" href="/favicon.ico" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="black-translucent"
